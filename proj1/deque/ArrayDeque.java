795143cd3d8c2afc8a7e4d7324285e67970d4fa6
hxt616 <1959490151@qq.com>
2024-05-24 15:39:45 +0800

    first commit

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -67,7 +67,7 @@ public class ArrayDeque<T> {
 
     public T removeFirst() {
 
-        if(size < list.length/4 && (size > 8)) {
+        if(size < list.length/4 && (size > 16)) {
             resizing(size-1);
         }
 
